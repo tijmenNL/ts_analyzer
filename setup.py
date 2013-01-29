ae8588b92775a6d40d8ddd7aeeb1bb25f7653061
Tijmen de Mes <tijmen@vck.utwente.nl>
2013-01-29 12:43:33 +0100

    Cleaning and add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='ts_analyser', 
+      version          = '0.1', 
+      author           = 'Tijmen de Mes',
+      author_email     = 'tijmen@vck.utwente.nl',
+      description      = "A TS analyser",
+      license          = "GPL",
+      platforms        = ["Platform Independent"],
+      classifiers      = [
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Cable Operators",
+          "License :: GNU General Public License (GPL)",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+      ],)
