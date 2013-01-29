969a958bc604adf3b83b5a2c2d88a5ecf2198a62
Tijmen de Mes <tijmen@vck.utwente.nl>
2013-01-29 16:32:14 +0100

    Cleaning and add setup.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+ """ts_analyzer"""
+ 
+ __version__ = '0.1'
+  
+ configuration_filename = "config.ini"
--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
+import glob
+import os
+import re
+
 from distutils.core import setup
 
+
+def get_version():
+    return re.search(r"""__version__\s+=\s+(?P<quote>['"])(?P<version>.+?)(?P=quote)""", open('__init__.py').read()).group('version')
+
 setup(name='ts_analyser', 
-      version          = '0.1', 
+      version          = get_version(), 
       author           = 'Tijmen de Mes',
       author_email     = 'tijmen@vck.utwente.nl',
       description      = "A TS analyser",
