657d198ed835b5b36eec917c5bcdb86a76d13eb9
Tijmen de Mes <tijmen@vck.utwente.nl>
2013-01-30 15:07:40 +0100

    bugfix

--- ts_analyzer.py
+++ ts_analyzer.py
@@ -77,7 +77,7 @@ def on_read(handle, ip_port, data, error):
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.render('index.html',(version=ts_analyzer.__version__,addresses=dict(addresses))
+        self.render('index.html',version=ts_analyzer.__version__,addresses=dict(addresses))
 
 
 class ChannelHandler(tornado.web.RequestHandler):
